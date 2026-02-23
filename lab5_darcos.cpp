43e62ce9f77e777b797c106672874081d9f7ea00
darcos5 <darcos@student.cscc.edu>
2026-02-22 21:57:31 -0500

    Copied corrected code from lab 4 submission & fix header

--- lab5_darcos.cpp
+++ lab5_darcos.cpp
@@ -1,36 +1,35 @@
 /**
- * @file lab4_darcos.cpp
+ * @file lab5_darcos.cpp
  * @author Daisy Arcos
- * @date 2026-02-15
- * @brief a program to generate a multiplication table with valid user input 
+ * @date 2026-02-22
+ * @brief a program to generate a multiplication table with valid user input and refactorized 
  */
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    int maxDigit; 
-	
-    cout << "Please enter the maximum digit for the multiplication table." << endl;
-    cout << "The digit must be greater than 4 and less than 10." << endl;
-    cout << "Max Digit: ";
-    cin >> maxDigit; 
+  int maxDigit;
 
-    //input validation loop
-    //still need to add condition for non integer values 
-    while (maxDigit<4 || maxDigit>10){
-        cout << "Error: The max digit must be greater than 4 and less than 10. Please try again." << endl;
-            cout << "Max Digit: ";
-            cin >> maxDigit;
-            
-            if (maxDigit>4 && maxDigit<10){
-                for (int i = 1 ; i <= maxDigit; i++){
-                    for(int j = 1; j <= maxDigit; j++){
-                        cout << i*j << "\t";
-                    }
-                    cout<<endl; 
-                }
-            } 
+  cout << "Please enter the maximum digit for the multiplication table." << endl;
+  cout << "The digit must be greater than 4 and less than 10." << endl;
+  cout << "Max Digit: ";
+  cin >> maxDigit;
+
+  //input validation loop
+  //still need to add condition for non integer values 
+  while (maxDigit <= 4 || maxDigit >= 10) {
+    cout << "Error: The max digit must be greater than 4 and less than 10. Please try again." << endl;
+    cout << "Max Digit: ";
+    cin >> maxDigit;
+  }
+
+  for (int i = 1; i <= maxDigit; i++) {
+    for (int j = 1; j <= maxDigit; j++) {
+      cout << i * j << "\t";
     }
-	return 0;
+    cout << endl;
+  }
+
+  return 0;
 }
